62f66e12f8ed61d78c5e24dbcd2b6fa61198b5ee
Bennor McCarthy <bennor@gmail.com>
2026-07-14 17:37:38 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.MapOpenApi();
+
+app.MapGet("/", () => TypedResults.Json(new
+{
+  message = "Hi from .NET on Vercel",
+  datetime = DateTimeOffset.UtcNow
+}));
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+
+app.Run($"http://0.0.0.0:{port}");
